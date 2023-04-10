8d6886621ff9abc01f851bf60363750aab002ef6
Lucetesoft <msoh@lucetesoft.com>
2023-04-10 16:25:50 +0900

    Release 1.0.1

--- Sources/GitpleLiveChat/GitpleLiveChat.swift
+++ Sources/GitpleLiveChat/GitpleLiveChat.swift
@@ -1,6 +1,31 @@
-public struct GitpleLiveChat {
-    public private(set) var text = "Hello, World!"
+//
+//  ChatClient.swift
+//  GitpleLive SDK Sample App
+//
+//  Created by Lucetesoft Inc. on 2023/02/23.
+//
 
-    public init() {
+import Foundation
+
+public class GitpleLiveChat: ChatClientSdk {
+    
+    public var delegate: ConnectionDelegate? {
+        get {
+            return connectionEvent
+        }
+        set(value) {
+            connectionEvent = value
+        }
     }
+    
+    public static var shared: GitpleLiveChat!
+    
+    public static var user: UserApi = UserApi()
+    public static var groupChannel = GroupChannelApi()
+    public static var groupChannelMessage = GroupChannelMessageApi()
+    
+    public static func setup(host: String, appId: String) {
+        shared = GitpleLiveChat(host: host, appId: appId)
+    }
+    
 }
---
+++ Sources/GitpleLiveChat/GitpleLiveChatTest.swift
@@ -0,0 +1,6 @@
+public struct GitpleLiveChatTest {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
